60901e237751eafd466456337fe87ac0c9ba8e86
Tóth Evelin <tothevelin@Toth-MacBook-Pro.local>
2022-02-22 07:32:47 +0100

    iterate through loop in templates, add more templates and make the nav items highlighted

--- application/routes.py
+++ application/routes.py
@@ -5,17 +5,18 @@ from flask import render_template
 @app.route("/index")
 @app.route("/home")
 def index():
-    return render_template("index.html", login=False)
+    return render_template("index.html", login=False, index=True)
 
 @app.route("/poems")
 def poems():
-    return render_template("poems.html")
+    return render_template("poems.html", poemsData=[{"author":"Margaret Atwood", "title":"February"}, {"author":"Eleonor Farjeon", "title":"Cats sleep anywhere"}], poems=True)
 
 @app.route("/login")
 def login():
-    return "<h1>TO DO<h1>"
+    return render_template("login.html", login=True)
 
 @app.route("/register")
 def register():
-    return "<h1>TO DO<h1>"
-
+    return render_template("register.html", register=True)
+    
+    
