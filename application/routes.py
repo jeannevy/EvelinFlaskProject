241eadf7a7d48dcf30f467c89bf955422334e17d
Tóth Evelin <tothevelin@Toth-MacBook-Pro.local>
2022-02-23 17:52:17 +0100

    use url variables, create api and add reading function

--- application/routes.py
+++ application/routes.py
@@ -1,5 +1,8 @@
 from application import app
-from flask import render_template
+from flask import render_template, request, Response 
+import json
+
+poemsData = [{"author":"Margaret Atwood", "title":"February"}, {"author":"Eleonor Farjeon", "title":"Cats sleep anywhere"}]
 
 @app.route("/")
 @app.route("/index")
@@ -7,9 +10,11 @@ from flask import render_template
 def index():
     return render_template("index.html", login=False, index=True)
 
-@app.route("/poems")
-def poems():
-    return render_template("poems.html", poemsData=[{"author":"Margaret Atwood", "title":"February"}, {"author":"Eleonor Farjeon", "title":"Cats sleep anywhere"}], poems=True)
+@app.route("/poems/")
+@app.route("/poems/<century>")
+
+def poems(century="20th"):
+    return render_template("poems.html", poemsData=poemsData, poems=True, century=century)
 
 @app.route("/login")
 def login():
@@ -19,4 +24,19 @@ def login():
 def register():
     return render_template("register.html", register=True)
     
-    
+@app.route("/readpoem", methods=["GET", "POST"])
+def readpoem():
+    author = request.form.get('author')
+    title = request.form.get('title')
+    return render_template("readpoem.html", data={"author": author, "title": title})
+
+
+@app.route("/api/")
+@app.route("/api/<id>")
+def api(id=None):
+    if id == None:
+        jdata = poemsData
+    else:
+        jdata = poemsData[id]
+    return Response(json.dumps(jdata), mimetype="application/json") 
+
