af28aad4b6427b537c009ca8962cb3f32ea79c80
Tóth Evelin <tothevelin@Toth-MacBook-Pro.local>
2022-03-02 21:11:55 +0100

    add sqlite database connection, create data models and forms, add register and login logic

---
+++ application/models.py
@@ -0,0 +1,56 @@
+import flask
+from werkzeug.security import generate_password_hash, check_password_hash
+
+class User():
+
+    def __init__(self, first_name, last_name, email, password):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.password = generate_password_hash(password)
+        
+    @classmethod
+    def check_password(self, hassed_password, password):
+        print(hassed_password)
+        print(password)
+        return check_password_hash(hassed_password, password)    
+
+    def get_sql_insert_command_for_user(self):
+        return f'''INSERT INTO User (first_name, last_name, email, password)
+                                VALUES (\"{self.first_name}\", \"{self.last_name}\", \"{self.email}\", \"{self.password}\")'''
+        
+        
+class Poem():
+    def __init__(self, author, title, content):
+        self.author = author
+        self.title = title
+        self.content = content
+
+    def get_sql_insert_command_for_poem(self):
+        return f'''INSERT INTO Poem (author, title, content)
+                                VALUES (\"{self.author}\", \"{self.title}\", \"{self.content}\")'''
+                                
+    
+
+class Like():
+    email = ""
+    author = ""
+    title = ""
+
+
+def create_user_table():
+    return '''CREATE TABLE User (
+        first_name varchar(50) NOT NULL,
+        last_name varchar(50),
+        email varchar(50) NOT NULL UNIQUE,
+        password varchar(15)
+     );'''
+
+def create_poem_table():
+    return '''CREATE TABLE IF NOT EXISTS Poem (
+        author varchar(50) NOT NULL,
+        title varchar(50) NOT NULL,
+        content varchar(50) NOT NULL
+     );'''
+    
+                        
--- application/routes.py
+++ application/routes.py
@@ -1,6 +1,14 @@
 from application import app, db
-from flask import render_template, request, Response 
+from flask import render_template, request, Response, redirect, flash
 import json
+import sqlite3
+from flask import g
+from application.forms import LoginForm, RegisterForm
+from application import models
+
+DATABASE = 'testdb1.db'
+connection = sqlite3.connect(DATABASE, check_same_thread=False)
+cursor = connection.cursor()
 
 poemsData = [{"author":"Margaret Atwood", "title":"February"}, {"author":"Eleonor Farjeon", "title":"Cats sleep anywhere"}]
 
@@ -16,19 +24,53 @@ def index():
 def poems(century="20th"):
     return render_template("poems.html", poemsData=poemsData, poems=True, century=century)
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-    return render_template("login.html", login=True)
+    form = LoginForm()
+    if form.validate_on_submit():
+        email = form.email.data
+        password = form.password.data
+        first_name_and_password = cursor.execute(f"select first_name, password from User where email == \"{email}\"").fetchone()
+        if first_name_and_password and models.User.check_password(first_name_and_password[1], password):
+            flash(f"{first_name_and_password[0]}, you are successfully logged in!", "success")
+            return redirect("/index")            
+        else:
+            flash("Sorry, something went wrong.","danger")
+    return render_template("login.html", form=form, login=True)
 
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    return render_template("register.html", register=True)
+    form = RegisterForm()
+    if form.validate_on_submit():
+        email = form.email.data
+        password = form.password.data
+        first_name = form.first_name.data
+        last_name = form.last_name.data
+        user = models.User(first_name, last_name, email, password)
+        cursor.execute(user.get_sql_insert_command_for_user())
+        connection.commit()
+        flash("You are successfully registered!", "success")
+        return redirect("/index")
+    return render_template("register.html", form=form, register=True)
     
 @app.route("/readpoem", methods=["GET", "POST"])
 def readpoem():
+#   cursor.execute("delete from Poem")
+#   cursor.execute("delete from User")
+#   
+#   connection.commit()
+#   cursor.execute(models.create_poem_table())
+#   connection.commit()
+#   poem = models.Poem("Margaret Atwood", "February", "Winter. Time to eat fat \n and watch hockey. In the pewter mornings, the cat, \n a black fur sausage with yellow \n Houdini eyes, jumps up on the bed and tries")
+#   cursor.execute(poem.get_sql_insert_command_for_poem())
+#   poem2 = models.Poem("Eleonor Farjeon", "Cats sleep anywhere", "Fitted in a cardboard box, \n In the cupboard with your frocks \n Anywhere! They don’t care! Cats sleep anywhere!")
+#   cursor.execute(poem2.get_sql_insert_command_for_poem())
+#   connection.commit()
     author = request.form.get('author')
     title = request.form.get('title')
-    return render_template("readpoem.html", data={"author": author, "title": title})
+    content = cursor.execute(f"select content from Poem where author = \"{author}\" and title = \"{title}\"").fetchone()
+    
+    return render_template("readpoem.html", data={"author": author, "title": title, 'content': content[0].split('\n')})
 
 
 @app.route("/api/")
@@ -40,13 +82,18 @@ def api(id=None):
         jdata = poemsData[id]
     return Response(json.dumps(jdata), mimetype="application/json") 
 
-class User(db.Document):
-    id = db.IntField( unique=True )
-    name =  db.StringField( max_length=50 )
-
+#class User(db.Document):
+#   id = db.IntField( unique=True )
+#   name =  db.StringField( max_length=50 )
+#
 @app.route("/user")
 def user():
-    User(id=11, name='Bett').save()
-    User(id=3, name='Gergo').save()
-    users = User.objects.all()
-    return render_template("user.html", users=users)
+#   User(id=11, name='Bett').save()
+#   User(id=3, name='Gergo').save()
+#   users = User.objects.all()
+#   cursor.execute(models.create_user_table())
+#   cursor.execute(models.insert_to_user())
+#   connection.commit()
+#   connection.close()
+    
+    return render_template("user.html", users=[{'id':1, 'name':'Boro'}, {'id':2, 'name':"Felcsi"}])
